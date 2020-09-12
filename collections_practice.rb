0e7630c7816191554a5ad620961729ac52d98d71
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 20:03:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end 
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def sum_array(array)
+  array.inject(0) do |sum,x| sum + x 
+  end
+end
+
+def kesha_maker (array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
+
+def find_a (array)
+  array.select do |word|
+    word.chr == "a"
+  end
+end
+
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
