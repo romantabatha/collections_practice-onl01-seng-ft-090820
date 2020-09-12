a1da5f205b446a1289bdd42168aaa58fc9459b23
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 19:40:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,42 +1,42 @@
-def sort_array_asc(array)
-  array.sort do |a,b|
-    a <=> b
-  end
-end
+# def sort_array_asc(array)
+#   array.sort do |a,b|
+#     a <=> b
+#   end
+# end
 
-def sort_array_desc(array)
-  array.sort do |a,b|
-    b <=> a
-  end
-end
+# def sort_array_desc(array)
+#   array.sort do |a,b|
+#     b <=> a
+#   end
+# end
 
-def sort_array_char_count(array)
-  array.sort do |a,b|
-    a.length <=> b.length
-  end 
-end
+# def sort_array_char_count(array)
+#   array.sort do |a,b|
+#     a.length <=> b.length
+#   end 
+# end
 
-def swap_elements(array)
+# def swap_elements(array)
   
-end
+# end
 
-def reverse_array
+# def reverse_array
   
-end
+# end
 
-def kesha_maker
+# def kesha_maker
   
-end
+# end
 
-def find_a
+# def find_a
   
-end
+# end
 
 def sum_array
-  
+  array.inject(0){|sum,x| sum + x }
 end
 
-def add_s
+# def add_s
   
-end
+# end
 
