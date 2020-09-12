07fcb9fb1604b8dce06c44803736628823bed47e
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 20:01:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,8 +25,11 @@ def sum_array(array)
   end
 end
 
-def kesha_maker(array)
-  
+def kesha_maker (array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
 end
 
 def find_a(array)
