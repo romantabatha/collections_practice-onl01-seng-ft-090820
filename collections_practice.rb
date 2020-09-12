ee486b227a0b552dc1a3c1b9ead9b9e6885503dd
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 19:54:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,9 +16,6 @@ def sort_array_char_count(array)
   end 
 end
 
-def swap_elements(array)
-  
-end
 
 def reverse_array(array)
   array.reverse
@@ -42,3 +39,6 @@ def add_s(array)
   
 end
 
+def swap_elements(array)
+  
+end
